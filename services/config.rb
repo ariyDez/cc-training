ba66d367d5bfbc77d657767de3bbb9e3f6c36755
nursultan <nursultan@cloudcoure.com>
2016-12-16 15:00:00 +0600

    changed config.yaml and config.rb

--- services/config.rb
+++ services/config.rb
@@ -11,16 +11,23 @@
 	## end
 	##
 
-coreo_aws_advisor_alert "test_alert" do
-	action :define
-	service :elb
-	display_name "Heartbeat check"
-	description "This check finds out what load balancers have healthcheck intervals > 2 mins"
-	level "Informational"
-	category "Inventory"
-	suggested_action "None."
-	objectives ["load_balancers"]
-	audit_objects ["load_balancer_description.health_check.interval"]
-	operators [">"]
-	alert_when [120]
+coreo_aws_advisor_alert "elb-inventory" do
+  action :define
+  service :elb
+  #include_violations_in_count false
+  display_name "ELB Object Health Check"
+  description "Load balancer health check is greater then 2 min."
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["load_balancers"]
+  audit_objects ["load_balancer_descriptions.health_check.interval"]
+  operators [">"]
+  alert_when [1]
+end
+
+coreo_aws_advisor_elb "advise-elb" do
+  alerts ${AUDIT_AWS_ELB_ALERT_LIST}
+  action :advise
+  regions ${AUDIT_AWS_ELB_REGIONS}
 end
